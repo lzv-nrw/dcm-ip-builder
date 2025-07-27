3fa2bc708d78ecac1b12086d01f3de8d1c079eb1
RichtersFinger <steffen.finger.privat@gmail.com>
2025-07-27 11:27:14 +0200

    [package] add 'bagit-utils' to dependencies

--- setup.py
+++ setup.py
@@ -11,6 +11,7 @@ setup(
     install_requires=[
         "flask==3.*",
         "PyYAML==6.*",
+        "bagit-utils>=1.1.1,<2.0.0",
         "bagit>=1.7.0,<2.0.0",
         "bagit_profile>=1.3.1,<2.0.0",
         "lxml==5.*",
